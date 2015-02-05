39ce4168bf17bb64c750199e6449ebc031bc5701
Stuart Olivera <stuart@stuartolivera.com>
2015-02-04 21:29:55 -0500

    Initialized Xcode project

---
+++ Pegg/Pegg/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pegg
+//
+//  Created by Stuart Olivera on 2/4/15.
+//  Copyright (c) 2015 Henry Saniuk, Stuart Olivera, Brandon Hudson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
