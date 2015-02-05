4c67ee05a9da5db8de9e816f5f289f782458e519
Henry Saniuk <henry@henrysaniuk.com>
2015-02-04 23:05:10 -0500

    Added ProfileViewController

---
+++ Pegg/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  Pegg
+//
+//  Created by Henry Saniuk on 2/4/15.
+//  Copyright (c) 2015 Henry Saniuk, Stuart Olivera, Brandon Hudson. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
