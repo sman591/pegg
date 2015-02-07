3385d15ffbbc27e3abdae2a47fbaef1cec08ba2f
Henry Saniuk <henry@henrysaniuk.com>
2015-02-06 20:54:13 -0500

    Changes tableview data on profile page

--- Pegg/ProfileViewController.swift
+++ Pegg/ProfileViewController.swift
@@ -11,19 +11,54 @@ import Alamofire
 import SwiftyJSON
 import SwiftKeychainWrapper
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var profilePictureView: UIImageView!
     @IBOutlet weak var points: UILabel!
     @IBOutlet weak var name: UILabel!
     
+    var items: [String] = []
+    
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            //Badges
+            badgesTable()
+        case 1:
+            //Friends
+            friendsTable()
+        case 2:
+            //Recent
+            friendsTable()
+        default:
+            break
+        }
+    }
+    
+    func badgesTable() {
+        items = ["This", "Is", "Badges"]
+        tableView.reloadData()
+    }
+    
+    func friendsTable() {
+        items = ["This", "Is", "Friends"]
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        badgesTable()
         
         let image = UIImageView(image: UIImage(named: "headshot"))
         self.profilePictureView = image
         self.profilePictureView.layer.cornerRadius = self.profilePictureView.frame.size.width / 2;
         self.profilePictureView.clipsToBounds = true;
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
     }
     
@@ -53,7 +88,22 @@ class ProfileViewController: UIViewController {
                 }
         }
 
-
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
     }
     
     override func didReceiveMemoryWarning() {
